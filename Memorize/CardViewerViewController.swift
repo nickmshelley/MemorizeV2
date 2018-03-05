76e2d7ac46d100e676a4600e54bc85bf958ca4ed
Nick Shelley <nickmshelley@gmail.com>
2018-03-04 22:33:59 -0700

    Add title label and card shadow.

--- Memorize/CardViewerViewController.swift
+++ Memorize/CardViewerViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 import Anchorage
+import QuartzCore
 
 class CardViewerViewController: UIViewController {
     private let card: Card
+    private let titleLabel = UILabel(frame: .zero)
     private let questionView: FlippableTextView
     private let answerView: FlippableTextView
     private var isShowingQuestion = true
@@ -29,6 +31,7 @@ class CardViewerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Cards"
         configureView()
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tap))
@@ -45,18 +48,36 @@ class CardViewerViewController: UIViewController {
 
 extension CardViewerViewController {
     func configureView() {
-        let container = UIView(frame: .zero)
-        [questionView, answerView].forEach { container.addSubview($0) }
-        view.addSubview(container)
+        let questionAnswerContainer = UIView(frame: .zero)
+        [questionView, answerView].forEach { questionAnswerContainer.addSubview($0) }
+        view.addSubview(titleLabel)
+        view.addSubview(questionAnswerContainer)
         answerView.isHidden = true
         
-        view.backgroundColor = .gray
+        view.backgroundColor = .lightGray
+        titleLabel.text = "Question"
+        titleLabel.font = UIFont.systemFont(ofSize: 28)
+        titleLabel.textAlignment = .center
+        questionView.layer.cornerRadius = 5
+        questionView.clipsToBounds = false
+        questionView.layer.shadowOffset = CGSize(width: -2, height: 2)
+        questionView.layer.shadowRadius = 2
+        questionView.layer.shadowOpacity = 0.5
+        answerView.layer.cornerRadius = 5
+        answerView.clipsToBounds = false
+        answerView.layer.shadowOffset = CGSize(width: -2, height: 2)
+        answerView.layer.shadowRadius = 2
+        answerView.layer.shadowOpacity = 0.5
         
-        container.edgeAnchors == view.edgeAnchors
-        questionView.edgeAnchors >= container.safeAreaLayoutGuide.edgeAnchors + 20
-        answerView.edgeAnchors >= container.safeAreaLayoutGuide.edgeAnchors + 20
-        questionView.centerYAnchor == container.centerYAnchor
-        answerView.centerYAnchor == container.centerYAnchor
+        titleLabel.horizontalAnchors == view.safeAreaLayoutGuide.horizontalAnchors + 16
+        titleLabel.topAnchor == view.safeAreaLayoutGuide.topAnchor + 16
+        titleLabel.bottomAnchor == questionAnswerContainer.topAnchor
+        questionAnswerContainer.horizontalAnchors == view.safeAreaLayoutGuide.horizontalAnchors
+        questionAnswerContainer.bottomAnchor == view.safeAreaLayoutGuide.bottomAnchor
+        questionView.edgeAnchors >= questionAnswerContainer.edgeAnchors + 16
+        answerView.edgeAnchors >= questionAnswerContainer.edgeAnchors + 16
+        questionView.centerYAnchor == questionAnswerContainer.centerYAnchor
+        answerView.centerYAnchor == questionAnswerContainer.centerYAnchor
         questionHeightConstraint = questionView.heightAnchor == 50 ~ .high
         answerHeightConstraint = answerView.heightAnchor == 50 ~ .high
     }
@@ -64,8 +85,10 @@ extension CardViewerViewController {
     @objc func tap() {
         if isShowingQuestion {
             UIView.transition(from: questionView, to: answerView, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+            UIView.transition(with: titleLabel, duration: 0.5, options: .transitionCrossDissolve, animations: { self.titleLabel.text = "Answer" }, completion: nil)
         } else {
             UIView.transition(from: answerView, to: questionView, duration: 0.5, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+            UIView.transition(with: titleLabel, duration: 0.5, options: .transitionCrossDissolve, animations: { self.titleLabel.text = "Question" }, completion: nil)
         }
         
         isShowingQuestion = !isShowingQuestion
