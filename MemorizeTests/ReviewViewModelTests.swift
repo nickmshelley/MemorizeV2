5d509be83af2c8b903f94e12feec5cf53112e53b
Nick Shelley <nickmshelley@gmail.com>
2018-05-13 09:18:48 -0600

    Add view model test for correct.

--- MemorizeTests/ReviewViewModelTests.swift
+++ MemorizeTests/ReviewViewModelTests.swift
@@ -28,6 +28,44 @@ class ReviewViewModelTests: XCTestCase {
         XCTAssertEqual(vm.question, "A")
         XCTAssertEqual(vm.answer, "B")
     }
+    
+    func testCorrect() {
+        UserDataController.shared = UserDataController(path: nil)
+        let needsReview = Date().addingTimeInterval(-50)
+        createCard(normalNextReview: needsReview, reverseNextReview: needsReview)
+        createCard(normalNextReview: needsReview, reverseNextReview: needsReview)
+        
+        let vm = ReviewViewModel()
+        XCTAssertTrue(vm.isNormal)
+        XCTAssertEqual(vm.remaining, 2)
+        
+        var beforeID = vm.currentCard!.id
+        vm.correct()
+        XCTAssertEqual(vm.remaining, 1)
+        XCTAssertNotEqual(vm.currentCard!.id, beforeID)
+        XCTAssertFalse(UserDataController.shared!.normalReadyToReviewCards().contains { $0.id == beforeID })
+        
+        beforeID = vm.currentCard!.id
+        vm.correct()
+        XCTAssertFalse(vm.isNormal)
+        XCTAssertEqual(vm.remaining, 2)
+        XCTAssertNotEqual(vm.currentCard!.id, beforeID)
+        XCTAssertFalse(UserDataController.shared!.normalReadyToReviewCards().contains { $0.id == beforeID })
+        
+        beforeID = vm.currentCard!.id
+        vm.correct()
+        XCTAssertEqual(vm.remaining, 1)
+        XCTAssertNotEqual(vm.currentCard!.id, beforeID)
+        XCTAssertFalse(UserDataController.shared!.reverseReadyToReviewCards().contains { $0.id == beforeID })
+        
+        beforeID = vm.currentCard!.id
+        vm.correct()
+        XCTAssertEqual(vm.remaining, 0)
+        XCTAssertNotEqual(vm.currentCard!.id, beforeID)
+        XCTAssertFalse(UserDataController.shared!.reverseReadyToReviewCards().contains { $0.id == beforeID })
+        
+        XCTAssertNil(vm.currentCard)
+    }
 }
 
 extension ReviewViewModelTests {
@@ -37,5 +75,17 @@ extension ReviewViewModelTests {
         try! UserDataController.shared!.createCard(id: id, question: "A", answer: "B", isReviewing: true, normalSuccessCount: 1, reverseSuccessCount: 1, normalNextReviewDate: normalNextReview, reverseNextReviewDate: reverseNextReview)
         
         return id
+    }
+    
+    private func verifyCorrect(viewModel vm: ReviewViewModel) {
+        let beforeRemaining = vm.remaining
+        let beforeID = vm.currentCard!.id
+        let isNormal = vm.isNormal
+        vm.correct()
+        
+        XCTAssertEqual(vm.remaining, beforeRemaining - 1)
+        XCTAssertNotEqual(vm.currentCard!.id, beforeID)
+        let newReviewing = UserDataController.shared!.reviewingCards()
+        XCTAssertFalse(newReviewing.contains { $0.id == beforeID })
     }
 }
